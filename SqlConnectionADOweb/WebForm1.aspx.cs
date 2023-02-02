83ba9fa278421d55a6f3c069573144e391f1709f
jogindrakumar <joginder9868@outlook.com>
2023-02-02 06:20:24 +0530

    Add project files.

---
+++ SqlConnectionADOweb/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SqlConnectionADOweb
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = "data source=VERONICA; database=Sample; integrated security=SSPI";
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+               
+            }
+
+            
+
+        }
+    }
+}
